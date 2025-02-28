ae4a3b878dcb50dc23b99e2880bf0654ef0c237c
William Pinilla <wills89pinilla@gmail.com>
2025-02-28 21:51:27 +0000

    Removed unnecessary dist

--- src/gulp-tasks/vendors.js
+++ src/gulp-tasks/vendors.js
@@ -10,7 +10,7 @@ gulp.task('clean:vendors', function () {
     ]);
 });
 
-/*Building vendor scripts needed for basic template rendering*/
+/* Build vendor scripts needed for basic template rendering */
 gulp.task('buildBaseVendorScripts', function () {
     return gulp.src([
         '../node_modules/jquery/dist/jquery.min.js',
@@ -22,14 +22,14 @@ gulp.task('buildBaseVendorScripts', function () {
         .pipe(gulp.dest('./assets/vendors/js'));
 });
 
-/*Building vendor styles needed for basic template rendering*/
+/* Build vendor styles needed for basic template rendering */
 gulp.task('buildBaseVendorStyles', function () {
     return gulp.src(['../node_modules/perfect-scrollbar/css/perfect-scrollbar.css'])
         .pipe(concat('vendor.bundle.base.css'))
         .pipe(gulp.dest('./assets/vendors/css'));
 });
 
-/*Scripts for addons*/
+/* Scripts for addons */
 gulp.task('buildOptionalVendorScripts', function () {
     var aScript1 = gulp.src(['../node_modules/chart.js/dist/chart.umd.js'])
         .pipe(gulp.dest('./assets/vendors/chart.js'));
@@ -51,11 +51,10 @@ gulp.task('buildOptionalVendorScripts', function () {
         .pipe(gulp.dest('./assets/vendors/codemirror'));
     var aScript59 = gulp.src(['../node_modules/owl-carousel-2/owl.carousel.min.js'])
         .pipe(gulp.dest('./assets/vendors/owl-carousel-2'));
-    return merge(aScript1, aScript23, aScript29, aScript33, aScript38, aScript39, aScript40, aScript41, aScript42, aScript59)
+    return merge(aScript1, aScript23, aScript29, aScript33, aScript38, aScript39, aScript40, aScript41, aScript42, aScript59);
 });
 
-
-/*Styles for addons*/
+/* Styles for addons */
 gulp.task('buildOptionalVendorStyles', function () {
     var aStyle1 = gulp.src(['../node_modules/@mdi/font/css/materialdesignicons.min.css'])
         .pipe(gulp.dest('./assets/vendors/mdi/css'));
@@ -88,11 +87,11 @@ gulp.task('buildOptionalVendorStyles', function () {
     var aStyle54 = gulp.src(['../node_modules/pwstabs/assets/jquery.pwstabs.min.css'])
         .pipe(gulp.dest('./assets/vendors/pwstabs'));
     var fonts = gulp.src(['./assets/fonts/fonts/*'])
-        .pipe(gulp.dest('./assets/vendors/bootstrap-datepicker/fonts'))
-    return merge(aStyle1, aStyle2, aStyle3, aStyle4, aStyle9, aStyle10, aStyle32, aStyle34, aStyle35, aStyle36, aStyle37, aStyle45, aStyle46, aStyle47, aStyle54, fonts)
+        .pipe(gulp.dest('./assets/vendors/bootstrap-datepicker/fonts'));
+    return merge(aStyle1, aStyle2, aStyle3, aStyle4, aStyle9, aStyle10, aStyle32, aStyle34, aStyle35, aStyle36, aStyle37, aStyle45, aStyle46, aStyle47, aStyle54, fonts);
 });
 
-//Copy essential map files
+// Copy essential map files
 gulp.task('copyMapFiles', function () {
     var map1 = gulp.src('../node_modules/bootstrap/dist/js/bootstrap.min.js.map')
         .pipe(gulp.dest('./assets/vendors/js'));
@@ -101,5 +100,12 @@ gulp.task('copyMapFiles', function () {
     return merge(map1, map2);
 });
 
-/*sequence for building vendor scripts and styles*/
-gulp.task('bundleVendors', gulp.series('clean:vendors', 'buildBaseVendorStyles', 'buildBaseVendorScripts', 'buildOptionalVendorStyles', 'buildOptionalVendorScripts', 'copyMapFiles'));
+/* Sequence for building vendor scripts and styles */
+gulp.task('bundleVendors', gulp.series(
+  'clean:vendors', 
+  'buildBaseVendorStyles', 
+  'buildBaseVendorScripts', 
+  'buildOptionalVendorStyles', 
+  'buildOptionalVendorScripts', 
+  'copyMapFiles'
+));
